ed8e1ba32a87223001b02da4c5771e61047ccc89
Luiz Carraro <lacarraro@gmail.com>
2018-09-02 01:43:46 -0300

    Cria controller para model Sentimentos

---
+++ api/controllers/SentmentController.js
@@ -0,0 +1,11 @@
+/**
+ * SentmentController
+ *
+ * @description :: Server-side logic for managing Sentments
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
